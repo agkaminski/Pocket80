c59a8bb7abeaf90d0fe277d810f76c3933674946
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2025-12-21 00:53:44 +0100

    FW: Serial echo demo

--- fw/main.c
+++ fw/main.c
@@ -26,9 +26,8 @@ int main(void)
 	ctc_control(2, &ctc);
 #endif
 
-#if 1
 	/* Baud generator */
-	struct ctc_control ctc = {
+	static const struct ctc_control ctc_baud = {
 		.interrupt = 0,
 		.mode = ctc_timer,
 		.prescaler = ctc_prescaler_16,
@@ -37,30 +36,46 @@ int main(void)
 		.time = { .present = 1, .constant = 1 },
 		.reset = 0
 	};
-	ctc_control(1, &ctc);
-#endif
+	ctc_control(1, &ctc_baud);
 
 	sio_init();
-	for (unsigned int i = 0;; ++i) {
-		for (volatile uint16_t j = 0; j < 0x7fff; ++j) {
-			__asm nop __endasm;
-		}
-		sio_tx('H');
-		sio_tx('e');
-		sio_tx('l');
-		sio_tx('l');
-		sio_tx('o');
-		sio_tx(' ');
-		sio_tx('W');
-		sio_tx('o');
-		sio_tx('r');
-		sio_tx('l');
-		sio_tx('d');
-		sio_tx('!');
-		sio_tx('\r');
-		sio_tx('\n');
 
-		dl1414_buff[0] = '0' + (i % 10);
+	for (size_t i = 0; i < 12; ++i) {
+		sio_write(0, dl1414_buff[i]);
+	}
+
+	sio_write(0, '\r');
+	sio_write(0, '\n');
+
+	for (;;) {
+		uint8_t data = sio_read(0);
+		sio_write(0, data);
+
+		static const struct ctc_control ctc_beep_start = {
+			.interrupt = 0,
+			.mode = ctc_timer,
+			.prescaler = ctc_prescaler_16,
+			.edge = ctc_edge_falling,
+			.trigger = ctc_trigger_auto,
+			.time = { .present = 1, .constant = 114/2 },
+			.reset = 0
+		};
+		static const struct ctc_control ctc_beep_stop = {
+			.interrupt = 0,
+			.mode = ctc_timer,
+			.prescaler = ctc_prescaler_16,
+			.edge = ctc_edge_falling,
+			.trigger = ctc_trigger_auto,
+			.time = { .present = 0, .constant = 0 },
+			.reset = 1
+		};
+
+		ctc_control(2, &ctc_beep_start);
+		for (volatile int i = 0; i < 1000; ++i);
+		ctc_control(2, &ctc_beep_stop);
+
+		memmove(dl1414_buff + 1, dl1414_buff, 11);
+		dl1414_buff[0] = data;
 		dl1414_update();
 	}
 
